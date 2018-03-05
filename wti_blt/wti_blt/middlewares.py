64338fca76c004821538c75d68bdfd2bbca3225a
xp <xue_yun@foxmail.com>
2018-03-05 15:00:51 +0800

    随机user-agent，防止被禁止

--- wti_blt/wti_blt/middlewares.py
+++ wti_blt/wti_blt/middlewares.py
@@ -7,6 +7,28 @@
 
 from scrapy import signals
 
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+import random
+
+
+
+class MyUserAgentMiddleware(UserAgentMiddleware):
+    '''
+    设置User-Agent
+    '''
+
+    def __init__(self, user_agent):
+        self.user_agent = user_agent
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            user_agent=crawler.settings.get('MY_USER_AGENT')
+        )
+
+    def process_request(self, request, spider):
+        agent = random.choice(self.user_agent)
+        request.headers['User-Agent'] = agent
 
 class WtiBltSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
